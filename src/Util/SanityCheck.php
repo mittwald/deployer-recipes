6fefc46a0e951c3b18922642fe31c9d7c5426ccb
Martin Helmich <m.helmich@mittwald.de>
2024-02-09 14:09:02 +0100

    Add support for using app short ids

--- src/Util/SanityCheck.php
+++ src/Util/SanityCheck.php
@@ -13,10 +13,6 @@ class SanityCheck
      */
     public static function assertAppInstallationID(string $id): void
     {
-        if (preg_match('/^a-[a-z0-9]+$/', $id)) {
-            throw new \InvalidArgumentException('The provided app installation ID looks like a short ID. Please use the full ID (which should be a UUIDv4) instead.');
-        }
-
         if (preg_match('/^p-[a-z0-9]+$/', $id)) {
             throw new \InvalidArgumentException('The provided app installation ID looks like a _project_ short ID. Please provide an app ID, and make sure to use the full ID (which should be a UUIDv4).');
         }
