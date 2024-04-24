0d1f3f40a2211899dd254fd3fdd473617a5c76a0
Martin Helmich <m.helmich@mittwald.de>
2024-04-24 13:37:20 +0200

    Add CI pipelines for testing the actual deployment functionality

---
+++ functionaltest/deploy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/common.php';
+require 'contrib/rsync.php';
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/mittwald/deployer-recipes/recipes/deploy.php';
+
+// Config
+
+set('repository', 'https://github.com/martin-helmich/t3crr23-deployer-demo.git');
+set('rsync_src', __DIR__);
+set('php_version', '8.2');
+set('domain', getenv("MITTWALD_APP_DOMAIN"));
+
+// Hosts
+
+mittwald_app(getenv("MITTWALD_APP_ID"))
+    ->set('public_path', '{{typo3_webroot}}');
+
+// Hooks
+
+task('deploy:prepare', [
+    'deploy:info',
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    'deploy:shared',
+    'deploy:writable',
+]);
+
+after('deploy:failed', 'deploy:unlock');
